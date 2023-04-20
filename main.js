93854ea25802b6b0b101f297824c06a8ce73d6fe
Fatih Şen <fatihydrm@outlook.com>
2023-04-21 00:22:23 +0300

    fix: navbar menu

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-const navBtn = document.querySelector('.nav-btn');
-const nav = document.querySelector('#nav');
-const mainNav = document.querySelector('.main-nav')
-const navItems = document.querySelectorAll('.nav-item');
+const navBtn = document.querySelector(".nav-btn");
+const nav = document.querySelector("#nav");
+const mainNav = document.querySelector(".main-nav");
+const navItems = document.querySelectorAll(".nav-item");
 
 let showMenu = false;
 
-navBtn.addEventListener('click', toggleMenu);
+navBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    if (!showMenu) {
-        navBtn.classList.add('open');
-        nav.classList.add('open');
-        mainNav.classList.add('open')
-        navItems.forEach(item => item.classList.add('open'));
+   if (!showMenu) {
+      navBtn.classList.add("open");
+      nav.classList.add("open");
+      mainNav.classList.add("open");
+      navItems.forEach((item) => item.classList.add("open"));
 
-        showMenu = true;
-    }else {
-        navBtn.classList.remove('open');
-        nav.classList.remove('open');
-        mainNav.classList.remove('open')
-        navItems.forEach(item => item.classList.remove('open'));
+      showMenu = true;
+   } else {
+      navBtn.classList.remove("open");
+      nav.classList.remove("open");
+      mainNav.classList.remove("open");
+      navItems.forEach((item) => item.classList.remove("open"));
 
-        showMenu = false;
-    }
+      showMenu = false;
+   }
 }
 
 function closeMenu() {
-    mainNav.classList.remove("open");
+   navBtn.classList.remove("open");
+   nav.classList.remove("open");
+   mainNav.classList.remove("open");
+   navItems.forEach((item) => item.classList.remove("open"));
 }
 
 const homeLink = document.getElementById("home-link");
@@ -34,22 +37,22 @@ const aboutLink = document.getElementById("about-link");
 const portfolioLink = document.getElementById("portfolio-link");
 const contactLink = document.getElementById("contact-link");
 
-homeLink.addEventListener("click", function() {
-  document.getElementById("home").scrollIntoView();
-  closeMenu();
+homeLink.addEventListener("click", function () {
+   document.getElementById("home").scrollIntoView();
+   closeMenu();
 });
 
-aboutLink.addEventListener("click", function() {
-  document.getElementById("about").scrollIntoView();
-  closeMenu();
+aboutLink.addEventListener("click", function () {
+   document.getElementById("about").scrollIntoView();
+   closeMenu();
 });
 
-portfolioLink.addEventListener("click", function() {
-  document.getElementById("portfolio").scrollIntoView();
-  closeMenu();
+portfolioLink.addEventListener("click", function () {
+   document.getElementById("portfolio").scrollIntoView();
+   closeMenu();
 });
 
-contactLink.addEventListener("click", function() {
-  document.getElementById("contact").scrollIntoView();
-  closeMenu();
+contactLink.addEventListener("click", function () {
+   document.getElementById("contact").scrollIntoView();
+   closeMenu();
 });
