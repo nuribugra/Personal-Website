d644096d3b43081f03a8604ab729c191825fbb4d
Nuri Buğra Demir <bugranuri.demir@gmail.com>
2023-04-20 23:11:38 +0300

    Personal Website was completed

---
+++ README.md
@@ -0,0 +1,20 @@
+# Personal Website (Responsive)
+
+Before the intermediate Javascript lessons, I want to create a personal website for myself. 
+
+Project Details:
+- CSS Flexbox
+- CSS Variables
+- Basic Javascript
+- Rem Units
+- Responsive Design
+
+### PS: I prefer minimalism in everything. That's why I designed this website from a minimalist perspective. In my opinion, the website is in perfect condition in terms of accessibility and aesthetics.
+
+### Website link: 
+
+## Useful Resources That I Used This Project:
+- https://colorhunt.co/
+- https://getbootstrap.com/docs/5.0/layout/breakpoints/
+- https://fonts.google.com/
+- https://fontawesome.com/
---
+++ main.js
@@ -0,0 +1,56 @@
+const navBtn = document.querySelector('.nav-btn');
+const nav = document.querySelector('#nav');
+const mainNav = document.querySelector('.main-nav')
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+navBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        navBtn.classList.add('open');
+        nav.classList.add('open');
+        mainNav.classList.add('open')
+        navItems.forEach(item => item.classList.add('open'));
+
+        showMenu = true;
+    }else {
+        navBtn.classList.remove('open');
+        nav.classList.remove('open');
+        mainNav.classList.remove('open')
+        navItems.forEach(item => item.classList.remove('open'));
+
+        showMenu = false;
+    }
+}
+
+function closeMenu() {
+    let menu = document.querySelector(".main-nav");
+    menu.classList.remove("open");
+}
+
+const homeLink = document.getElementById("home-link");
+const aboutLink = document.getElementById("about-link");
+const portfolioLink = document.getElementById("portfolio-link");
+const contactLink = document.getElementById("contact-link");
+
+homeLink.addEventListener("click", function() {
+  document.getElementById("home").scrollIntoView();
+  closeMenu();
+});
+
+aboutLink.addEventListener("click", function() {
+  document.getElementById("about").scrollIntoView();
+  closeMenu();
+});
+
+portfolioLink.addEventListener("click", function() {
+  document.getElementById("portfolio").scrollIntoView();
+  closeMenu();
+});
+
+contactLink.addEventListener("click", function() {
+  document.getElementById("contact").scrollIntoView();
+  closeMenu();
+});
